f0d217181b4939dc5816ca6c61264bcfffb2903d
Harlan Kellaway <harlan@prolificinteractive.com>
2015-08-01 13:58:09 -0400

    Created first example

--- swift-functional-intro.playground/Contents.swift
+++ swift-functional-intro.playground/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/*** What is functional programming? ***/
+
+/* This is an unfunctional function */
+
+var a = 0
+
+func incrementUnfunctional() -> () {
+    a += 1
+}
+
+incrementUnfunctional()
+print(a) // a = 1
+
+/* This is a functional function */
+
+a = 0
+
+func incrementFunctional(num: Int) -> Int {
+    return num + 1
+}
+
+a = incrementFunctional(a)
+print(a) // a = 1
